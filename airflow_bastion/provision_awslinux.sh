0b5a3de9fb745639e09e9358ffa0d1179873c6da
Michael Smith <smithatlanta@gmail.com>
2019-10-16 15:09:23 -0400

    Bastion working

---
+++ airflow_bastion/provision_awslinux.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash -x
+
+set -e
+
+wget https://s3.amazonaws.com/turner-iso-artifacts/AlertLogicAgents/al-agent-LATEST-1.x86_64.rpm -O ~/al-agent_LATEST_amd64.rpm
+echo "------------------- download threat manager -------------------"
+
+sudo rpm -i ~/al-agent_LATEST_amd64.rpm
+echo "------------------- install threat manager -------------------"
+
+sudo systemctl enable al-agent.service
+
+rm ~/al-agent_LATEST_amd64.rpm
+echo "------------------- enable autostart of threat manager and remove deb-------------------"
