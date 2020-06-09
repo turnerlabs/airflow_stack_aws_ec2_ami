9ddedd2bb4873161e296d318fe31bb7ff7f877c1
Michael Smith <smithatlanta@gmail.com>
2020-06-09 13:48:59 -0400

    reduced down to the bare bones since ssm is already on the ami.

--- airflow_bastion/provision_awslinux.sh
+++ airflow_bastion/provision_awslinux.sh
@@ -5,18 +5,9 @@ set -e
 sudo yum update -y
 echo "------------------- package update complete -------------------"
 
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm -O /home/ec2-user/amazon-cloudwatch-agent.rpm
-
-sudo rpm -i /home/ec2-user/amazon-cloudwatch-agent.rpm
-echo "------------------- download aws logs -------------------"
-
 sudo systemctl status amazon-ssm-agent
-echo "------------------- start of awslogs complete -------------------"
-
 sudo systemctl enable amazon-ssm-agent
-
-rm /home/ec2-user/amazon-cloudwatch-agent.rpm
-echo "------------------- enable autostart of awslogs complete -------------------"
+echo "------------------- enable autostart of ssm agent complete -------------------"
 
 wget https://s3.amazonaws.com/turner-iso-artifacts/AlertLogicAgents/al-agent-LATEST-1.x86_64.rpm -O /home/ec2-user/al-agent_LATEST_amd64.rpm
 echo "------------------- download threat manager -------------------"
